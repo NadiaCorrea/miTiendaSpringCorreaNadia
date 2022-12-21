1f47b254590c0b98459cb5404d2edb5425b42dd3
NadiaCorrea <ncormor536@gmail.com>
2022-12-21 11:44:31 +0100

    adding initial set up

---
+++ src/main/java/com/jacaranda/miTiendaSpringCorreaNadia/MiTiendaSpringCorreaNadiaApplication.java
@@ -0,0 +1,13 @@
+package com.jacaranda.miTiendaSpringCorreaNadia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiTiendaSpringCorreaNadiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiTiendaSpringCorreaNadiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jacaranda/miTiendaSpringCorreaNadia/MiTiendaSpringCorreaNadiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jacaranda.miTiendaSpringCorreaNadia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiTiendaSpringCorreaNadiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
